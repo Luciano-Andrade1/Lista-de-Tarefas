8ed950c1660e1f1c310ae8e1e5dd66118409c0f5
Luciano-Andrade1 <duarteluciano947@gmail.com>
2025-04-04 20:26:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let botao = document.getElementById('button')
+let tarefa = document.getElementById('input-tarefa')
+let lista = document.getElementById('list')
+
+botao.addEventListener('click', function () {
+    let paragrafo = document.createElement('p')
+    lista.appendChild(paragrafo)
+    paragrafo.innerHTML = tarefa.value 
+    paragrafo.classList.add('estilo-paragrafo')
+    lista.appendChild(paragrafo)
+
+    paragrafo.addEventListener('click', function(){
+        paragrafo.style.textDecoration = 'line-through'
+    })
+    paragrafo.addEventListener('dbclick', function(){
+        paragrafo.removeChild(paragrafo)
+    })  
+
+})
