4fb9ea99f780eb654d296c2788083eb1520fc654
Luciano-Andrade1 <duarteluciano947@gmail.com>
2025-04-04 20:44:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 let botao = document.getElementById('button')
 let tarefa = document.getElementById('input-tarefa')
-let lista = document.getElementById('list')
+let lista  = document.getElementById('list')
 
-botao.addEventListener('click', function () {
+botao.addEventListener('click', function() {
     let paragrafo = document.createElement('p')
     lista.appendChild(paragrafo)
-    paragrafo.innerHTML = tarefa.value 
+    paragrafo.innerHTML = tarefa.value
     paragrafo.classList.add('estilo-paragrafo')
-    lista.appendChild(paragrafo)
+   paragrafo.addEventListener('click', function() {
+       paragrafo.style.textDecoration = 'line-through'
+   })
 
-    paragrafo.addEventListener('click', function(){
-        paragrafo.style.textDecoration = 'line-through'
-    })
-    paragrafo.addEventListener('dbclick', function(){
-        lista.removeChild(paragrafo)
-    })  
+paragrafo.addEventListener('dblclick', function() {
+    lista.removeChild(paragrafo)
+}
+)
+    
 
-})
+
+
+
+   })
